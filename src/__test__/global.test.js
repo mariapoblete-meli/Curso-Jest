e531d9db3188cb94696292678f0ba1b5c352f5e0
Maria Jose <mariajose.pobletemauro@mercadolibre.cl>
2021-06-18 11:47:25 -0400

    Clase 5 implementando promesas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,9 +1,13 @@
+//Clase Preparación del entorno con Jest
+// Prueba de String
 const text = "Hola Mundo";
 const fruits = ['manzana' , 'melon', 'banana'];
 test('Debe contener un texto', () => {
     expect(text).toMatch(/Mundo/);
 
 });
+
+//Clase Implementación pruebas para Boolean y Array
 
 test('Tenemos una banana?', () => {
     expect(fruits).toContain('banana');
@@ -26,3 +30,33 @@ test('Probar un callback', () => {
         expect(str).toBe('aloH');
     });
 });
+
+//Implementando pruebas a promesas
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if(!str) {
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    });
+};
+
+test('Probar una promesa', () => {
+    return reverseString2('Hola')
+    .then(string => {
+        expect(string).toBe('aloH');
+    })
+
+});
+
+test('Probar async/await', async () => {
+    const string = await reverseString2('Hola');
+    expect(string).toBe('aloH');
+});
+
+afterEach(() => console.log('Despues de cada prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'));
+
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
